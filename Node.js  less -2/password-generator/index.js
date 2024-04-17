372413b9f5f55cd326cac23e4c4991192f6075cc
moriah <snailans73@gmail.com>
2024-04-17 18:53:39 +0600

    nodejs-tasks

--- Node.js  less -2/password-generator/index.js
+++
@@ -1,7 +0,0 @@
-const cryptoRandomString = require('crypto-random-string');
-
-function generatePassword(length = 12) {
-  return cryptoRandomString({ length, type: 'alphanumeric' });
-}
-
-module.exports = generatePassword;
--- Node.js  less -2/password-generator/test-password.js
+++
@@ -1,6 +0,0 @@
-const generatePassword = require('password-generator');
-
-// Генерация пароля длиной 12 символов (по умолчанию)
-const password = generatePassword();
-
-console.log("Сгенерированный пароль:", password);
